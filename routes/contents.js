9133f1c9966965af96be22f03dabcb6c109d8631
JHJee <chlwlgp132@gamil.com>
2019-11-27 23:05:57 +0900

    updates on pages routing

--- routes/contents.js
+++ routes/contents.js
@@ -6,7 +6,7 @@ router.get('/', (req,res,next) => {
     res.redirect('/');
 });
 
-/* Individual User GET */
+/* User Home GET */
 router.get('/home', (req,res,next) => {
     let sess = req.session;
     if(sess.userid && sess.typeUser){
@@ -18,9 +18,45 @@ router.get('/home', (req,res,next) => {
     }
 });
 
-/* See GET */
+/* User Checkup Records GET */
+router.get('/checkups', (req,res,next) => {
+    let sess = req.session;
+    if(sess.userid && sess.typeUser){
+        res.render('contents/checkups');
+    }
+    else{
+        console.log('user page, access denied');
+        res.redirect('/users/login');
+    }
+});
 
-/* Medical Staff GET */
+/* User Prescription Records */
+router.get('/prescriptions', (req,res,next) => {
+    let sess = req.session;
+    if(sess.userid && sess.typeUser){
+        res.render('contents/prescriptions');
+    }
+    else{
+        console.log('user page, access denied');
+        res.redirect('/users/login');
+    }
+});
+
+/* User Operation Records */
+router.get('/operations', (req,res,next) => {
+    let sess = req.session;
+    if(sess.userid && sess.typeUser){
+        res.render('contents/operations');
+    }
+    else{
+        console.log('user page, access denied');
+        res.redirect('/users/login');
+    }
+});
+
+/* --------------------------------------------------------- */
+
+/* Medical Staff Home GET */
 router.get('/medical-staff', (req,res,next) => {
     let sess = req.session;
     if(sess.userid && sess.typeMedStaff){
@@ -38,6 +74,6 @@ router.post('/medical-staff', (req,res,next) => {
     res.redirect('/');
 });
 
-/* Write GET*/
+/* Medical Staff Record GET */
 
 module.exports = router;
