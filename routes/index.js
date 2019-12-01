e244c1ac84f06ef8ce78824d5f4307c8c4bcfbaf
JHJee <chlwlgp132@gmail.com>
2019-12-01 10:45:45 +0000

    add features: filtering info, getting vital signs sesnsor as a JSON file

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+// Moment module for retrieving current time 
+var moment = require('moment');
+require('moment-timezone');
+moment.tz.setDefault('Asia/Seoul');
+
+// Multer settings
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/') // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+    },
+    filename: function (req, file, cb) {
+        let uploadTime = moment().format('YYYY-MM-DD_HH:mm:ss');
+        let extension = path.extname(file.originalname);
+        let basename = path.basename(file.originalname, extension);
+        cb(null, basename + '_' + uploadTime + extension); // cb 콜백함수를 통해 전송된 파일 이름 설정
+    }
+});
+var upload = multer({ storage: storage });
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
 });
+
+/* Upload GET*/
+router.get('/upload', (req, res, next) => {
+    res.render('upload');
+});
+
+/* Upload POST*/
+router.post('/upload', upload.single('jsonfile'), (req, res, next) => {
+    let uploadTime = moment().format('YYYY-MM-DD HH:mm:ss');
+
+    try {
+        res.send('File uploaded successfully' + '\n' + JSON.stringify(req.file));
+        console.log('File uploaded:' + '\n' + JSON.stringify(req.file) + 'Time: ' + uploadTime);
+    } catch (err) {
+        res.send(400);
+    }
+});
+
 module.exports = router;
